663aff03be783c2bb0fabd3dbea68f95e5ec20f9
Kevin Trieu <kevin.trieu@pheedloop.com>
2019-07-17 16:11:04 -0400

    Make it so only admins can access the editor

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from blog.models import Post
 from django.http.response import HttpResponse
+from django.contrib.admin.views.decorators import staff_member_required
+
+from blog.models import Post
 
 # Create your views here.
 
@@ -8,6 +10,7 @@ def posts(request):
     posts = Post.objects.filter(published=True)
     return render(request, 'blog/posts.html', context={ 'posts': posts })
 
+@staff_member_required
 def edit_post(request, **kwargs):
     post_id = kwargs['id']
     return HttpResponse(post_id)
